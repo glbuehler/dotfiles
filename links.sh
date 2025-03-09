3d8da3c88de35207e2a1db39b8ff9509954afc0d
Gideon Bühler <gideonbuehler18@gmail.com>
2025-03-09 22:10:43 +0100

    added i3

--- links.sh
+++ links.sh
@@ -6,3 +6,6 @@ ln -sf ~/dotfiles/ghostty/config ~/.config/ghostty/config
 
 mkdir -p ~/.config/tmux
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
+
+mkdir -p ~/.config/i3
+ln -sf ~/dotfiles/i3/config ~/.config/i3/config
