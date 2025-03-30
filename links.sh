758a1fccd14915c59493b140dfac02b934f07c35
Gideon Bühler <gideonbuehler18@gmail.com>
2025-03-31 01:54:29 +0200

    added gtk config

--- links.sh
+++ links.sh
@@ -12,5 +12,8 @@ ln -sf ~/dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
 mkdir -p ~/.config/i3
 ln -sf ~/dotfiles/i3/config ~/.config/i3/config
 
+mkdir -p ~/.config/gtk-3.0
+ln -sf ~/dotfiles/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+
 mkdir -p ~/.config
 ln -sdf ~/dotfiles/nvim/ ~/.config/nvim
