8d967c9850185dfbda886a646892bdc4b5d6f914
Gideon Bühler <gideonbuehler18@gmail.com>
2025-04-12 12:52:21 +0200

    map telescope buffers regardless of lsp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -20,7 +20,6 @@ return {
                 map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
                 map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
                 map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-                map('<leader><leader>', require('telescope.builtin').buffers, 'Find existing buffers')
                 map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
                 map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
                 map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
