3585b98b6efa701065a7316dd892eed0b2f21fdc
Gideon Bühler <gideonbuehler18@gmail.com>
2025-07-12 21:21:25 +0200

    add ToggleFmtOnSave usercommand

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,6 +3,39 @@ return {
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
+    config = function()
+        vim.api.nvim_create_user_command('ToggleFmtOnSave', function()
+            vim.b.format_on_save = not vim.b.format_on_save
+        end, {})
+
+        require('conform').setup({
+            notify_on_error = false,
+            format_on_save = function(bufnr)
+                if vim.b.format_on_save == nil then
+                    vim.b.format_on_save = true
+                end
+
+                local lsp_ignore = { 'c', 'cpp', 'typescript', 'java' }
+                if not vim.b.format_on_save or vim.tbl_contains(lsp_ignore, vim.bo[bufnr].filetype) then
+                    return
+                end
+
+                return {
+                    timeout_ms = 500,
+                    lsp_format = 'fallback',
+                }
+            end,
+            formatters_by_ft = {
+                lua = { 'stylua' },
+                html = { 'prettier' },
+                -- Conform can also run multiple formatters sequentially
+                -- python = { "isort", "black" },
+                --
+                -- You can use 'stop_after_first' to run the first available formatter from the list
+                -- javascript = { "prettierd", "prettier", stop_after_first = true },
+            },
+        })
+    end,
     keys = {
         {
             '<leader>f',
@@ -11,29 +44,6 @@ return {
             end,
             mode = '',
             desc = '[F]ormat buffer',
-        },
-    },
-    opts = {
-        notify_on_error = false,
-        format_on_save = function(bufnr)
-            local lsp_ignore = { 'c', 'cpp', 'typescript', 'java' }
-            if vim.tbl_contains(lsp_ignore, vim.bo[bufnr].filetype) then
-                return
-            end
-
-            return {
-                timeout_ms = 500,
-                lsp_format = 'fallback',
-            }
-        end,
-        formatters_by_ft = {
-            lua = { 'stylua' },
-            html = { 'prettier' },
-            -- Conform can also run multiple formatters sequentially
-            -- python = { "isort", "black" },
-            --
-            -- You can use 'stop_after_first' to run the first available formatter from the list
-            -- javascript = { "prettierd", "prettier", stop_after_first = true },
         },
     },
 }
